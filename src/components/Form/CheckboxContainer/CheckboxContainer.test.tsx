183efe8516071e62f55997ee8dda1457554ade82
Tristan Chin <tristan.chin@chintristan.io>
2021-07-11 21:49:48 -0400

    Add CheckboxContainer tests

--- src/components/Form/CheckboxContainer/CheckboxContainer.test.tsx
+++ src/components/Form/CheckboxContainer/CheckboxContainer.test.tsx
@@ -1,6 +1,9 @@
 import { createBasicTests } from "@utils/test";
-import CheckboxContainer from "./CheckboxContainer";
+import CheckboxContainer, {
+    extractCheckboxContainerProps,
+} from "./CheckboxContainer";
 import "jest-styled-components";
+import { CheckboxContainerPropsWithBase } from "./types";
 
 const { testClassName, testDisplayName, testRef, testRendering } =
     createBasicTests(CheckboxContainer, {});
@@ -10,4 +13,44 @@ describe("CheckboxContainer", () => {
     testDisplayName("NuiCheckboxContainer", "NuiStyledCheckboxContainer");
     testRef("div");
     testClassName("div", "NuiCheckboxContainer");
+});
+
+describe("extractCheckboxContainerProps", () => {
+    it("should extract the default CheckoutContainer props and put other props in the restProps", () => {
+        const customProps = { test: true } as const;
+        const { restProps, ...baseProps } = extractCheckboxContainerProps({
+            ...customProps,
+            disabled: undefined,
+        });
+        expect(restProps).toEqual(customProps);
+        expect(baseProps).toEqual({
+            disabled: false,
+            errors: [],
+            fluid: false,
+            focused: false,
+            label: "",
+            labelPosition: "right",
+            size: "sm",
+            touched: true,
+        });
+    });
+
+    it("should extract the CheckoutContainer props and put other props in the restProps", () => {
+        const inputBaseProps: Required<CheckboxContainerPropsWithBase> = {
+            disabled: true,
+            errors: ["test error"],
+            fluid: true,
+            focused: true,
+            label: "test label",
+            labelPosition: "bottom",
+            size: "xs",
+            touched: false,
+        };
+        const customProps = { test: true } as const;
+        const props = { ...inputBaseProps, ...customProps };
+        const { restProps, ...baseProps } =
+            extractCheckboxContainerProps(props);
+        expect(restProps).toEqual(customProps);
+        expect(baseProps).toEqual(inputBaseProps);
+    });
 });
