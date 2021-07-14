d8b3bced0b3d5d119dcfe4d6fb380c739ca52f9a
Tristan Chin <tristan.chin@chintristan.io>
2021-07-13 23:15:47 -0400

    Add Checkbox tests

--- src/components/Form/Checkbox/Checkbox.test.tsx
+++ src/components/Form/Checkbox/Checkbox.test.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { mount } from "enzyme";
 import { createBasicTests } from "@utils/test";
 import Checkbox from "./Checkbox";
 import "jest-styled-components";
@@ -10,4 +12,64 @@ describe("Checkbox", () => {
     testDisplayName("NuiCheckbox", "NuiStyledCheckbox");
     testRef("input");
     testClassName("div", "NuiCheckbox");
+
+    describe("onFocus", () => {
+        it("should call the onFocus handler", () => {
+            const onFocus = jest.fn();
+            const wrapper = mount(<Checkbox onFocus={onFocus} />);
+            const input = wrapper.find("input");
+
+            input.simulate("focus");
+            expect(onFocus).toHaveBeenCalled();
+        });
+    });
+
+    describe("onChange", () => {
+        it("should call the onChange handler", () => {
+            const onChange = jest.fn();
+            const wrapper = mount(<Checkbox onChange={onChange} />);
+            const input = wrapper.find("input");
+
+            input.simulate("focus");
+            input.simulate("change");
+            input.simulate("blur");
+            expect(onChange).toHaveBeenCalled();
+        });
+
+        it("should not call the onChange handler when it is readOnly", () => {
+            const onChange = jest.fn();
+            const wrapper = mount(<Checkbox onChange={onChange} readOnly />);
+            const input = wrapper.find("input");
+
+            input.simulate("change");
+            expect(onChange).not.toHaveBeenCalled();
+        });
+    });
+
+    describe("onBlur", () => {
+        it("should call the onBlur handler", () => {
+            const onBlur = jest.fn();
+            const wrapper = mount(<Checkbox onBlur={onBlur} />);
+            const input = wrapper.find("input");
+
+            input.simulate("blur");
+            expect(onBlur).toHaveBeenCalled();
+        });
+    });
+
+    describe("defaultChecked", () => {
+        it("should use the default checked value", () => {
+            const ref = React.createRef<HTMLInputElement>();
+            mount(<Checkbox ref={ref} />);
+
+            expect(ref.current?.checked).toBe(false);
+        });
+
+        it("should use the given defaultChecked value", () => {
+            const ref = React.createRef<HTMLInputElement>();
+            mount(<Checkbox ref={ref} defaultChecked />);
+
+            expect(ref.current?.checked).toBe(true);
+        });
+    });
 });
