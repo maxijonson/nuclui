f94d518af621e615c03f440225cacfafa0e2769e
Tristan Chin <tristan.chin@chintristan.io>
2021-07-25 19:15:58 -0400

    Add Switch tests

--- src/components/Form/Switch/Switch.test.tsx
+++ src/components/Form/Switch/Switch.test.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { mount } from "enzyme";
 import { createBasicTests } from "@utils/test";
 import Switch from "./Switch";
 import "jest-styled-components";
@@ -10,4 +12,80 @@ describe("Switch", () => {
     testDisplayName("NuiSwitch", "NuiStyledSwitch");
     testRef("input");
     testClassName("div", "NuiSwitch");
+
+    describe("onFocus", () => {
+        it("should call the onFocus handler", () => {
+            const onFocus = jest.fn();
+            const wrapper = mount(<Switch onFocus={onFocus} />);
+            const input = wrapper.find("input");
+
+            input.simulate("focus");
+            input.simulate("blur");
+            expect(onFocus).toHaveBeenCalled();
+        });
+    });
+
+    describe("onChange", () => {
+        it("should call the onChange handler", () => {
+            const onChange = jest.fn();
+            const wrapper = mount(<Switch onChange={onChange} />);
+            const input = wrapper.find("input");
+
+            input.simulate("change");
+            expect(onChange).toHaveBeenCalled();
+        });
+
+        it("should not call the onChange handler when it is readOnly", () => {
+            const onChange = jest.fn();
+            const wrapper = mount(<Switch onChange={onChange} readOnly />);
+            const input = wrapper.find("input");
+
+            input.simulate("change");
+            expect(onChange).not.toHaveBeenCalled();
+        });
+    });
+
+    describe("onBlur", () => {
+        it("should call the onBlur handler", () => {
+            const onBlur = jest.fn();
+            const wrapper = mount(<Switch onBlur={onBlur} />);
+            const input = wrapper.find("input");
+
+            input.simulate("focus");
+            input.simulate("blur");
+            expect(onBlur).toHaveBeenCalled();
+        });
+    });
+
+    describe("defaultChecked", () => {
+        it("should use the default checked value", () => {
+            const ref = React.createRef<HTMLInputElement>();
+            mount(<Switch ref={ref} />);
+
+            expect(ref.current?.checked).toBe(false);
+        });
+
+        it("should use the given defaultChecked value", () => {
+            const ref = React.createRef<HTMLInputElement>();
+            mount(<Switch ref={ref} defaultChecked />);
+
+            expect(ref.current?.checked).toBe(true);
+        });
+    });
+
+    describe("onChildren", () => {
+        it("should use the given on children", () => {
+            const wrapper = mount(<Switch value onChildren="On" />);
+            const text = wrapper.find(".NuiSwitch__text");
+            expect(text.text()).toBe("On");
+        });
+    });
+
+    describe("offChildren", () => {
+        it("should use the given off children", () => {
+            const wrapper = mount(<Switch value={false} offChildren="Off" />);
+            const text = wrapper.find(".NuiSwitch__text");
+            expect(text.text()).toBe("Off");
+        });
+    });
 });
